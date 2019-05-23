11233774b1f69f0c8262c210ce97f65f430c3c35
Emilio Cabrera <emilio1625@gmail.com>
2019-05-23 10:27:43 -0500

    Tabla de tipos

---
+++ proyecto/include/tabla_tipos.h
@@ -0,0 +1,83 @@
+#ifndef TYPE_TABLE_H
+#define TYPE_TABLE_H
+
+#include "intdef.h"
+#include "list.h"
+
+/**
+ * TT - tipos de datos primitivos del lenguaje de programacion
+ */
+enum TT {
+    TT_VOID,
+    TT_CHAR,
+    TT_INT,
+    TT_FLOAT,
+    TT_ARRAY,
+    TT_POINTER,
+    TT_STRUCT,
+};
+
+/**
+ * tipo - almacena la informacion de un tipo de dato del lenguaje
+ * @id: posicion en la tabla de tipos
+ * @tipo: tipo primitivo
+ * @base: tipos primitivo del que heredan los tipos compuestos
+ * @tam: tamaño en bytes que ocupa el tipo en memoria
+ * @dim: dimension del tipo compuesto
+ * @list: apuntadores al elemento anterior y siguiente en la tabla
+ */
+struct tipo {
+    u16 id;
+    enum TT tipo;
+    struct tipo *base;
+    u16 tam;
+    u8 dim;
+    struct list_head list;
+};
+
+/**
+ * tt_crear_tabla - aloja memoria y crea una nueva tabla de tipos con
+ *  los tipos primitivos
+ * @return: apuntador a la nueva tabla de tipos
+ */
+struct list_head *tt_crear_tabla();
+
+/**
+ * tt_eliminar_tabla - libera la memoria ocupada por una tabla de memoria
+ * @tt - la tabla a eliminar
+ */
+void tt_eliminar_tabla(struct list_head *tt);
+
+/**
+ * tt_insertar_tipo - aloja memoria y añade un nuevo tipo a la tabla de tipos
+ * @tt - tabla de tipos a la cual añadir
+ * @return - apuntador al nuevo tipo creado
+ *
+ * Ver tipo para mas informacion sobre los parametros
+ */
+struct tipo *tt_insertar_tipo(struct list_head *tt,
+                              enum TT tipo,
+                              struct tipo *base,
+                              u16 tam,
+                              u8 dim);
+
+/**
+ * tt_buscar_id - busca un tipo por su posicion en la tabla de tipos
+ * @tt - la tabla de tipos en la cual buscar
+ * @id - la posicion en la tabla de tipos a buscar
+ */
+struct tipo *tt_buscar_id(struct list_head *tt, u16 id);
+
+/**
+ * tt_imprimir_tipo - imprime los campos de un tipo a la consola
+ * @tipo - el tipo a imprimir
+ */
+void tt_imprimir_tipo(struct tipo *tipo);
+
+/**
+ * ts_imprimir_tabla - imprime una tabla de tipos
+ * @tt: la tabla a imprimir
+ */
+void tt_imprimir_tabla(struct list_head *tt);
+
+#endif /* end of include guard: TYPE_TABLE_H */
---
+++ proyecto/src/tabla_tipos.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "tabla_tipos.h"
+
+struct list_head *tt_crear_tabla()
+{
+    struct list_head *tt = list_new();
+    if (tt == NULL)
+        return NULL;
+    for (u8 i = 0; i < 4; i++) {
+        if (!tt_insertar_tipo(tt, i, NULL, (i < 2) ? i : 4, 0)) {
+            tt_eliminar_tabla(tt);
+            break;
+        }
+    }
+    return tt;
+}
+
+void tt_eliminar_tabla(struct list_head *tt)
+{
+    if (tt == NULL)
+        return;
+    struct tipo *tipo, *sig;
+    if (!list_empty(tt)) {
+        list_for_each_entry_safe(tipo, sig, tt, list)
+        {
+            list_del(&tipo->list);
+            free(tipo);
+        }
+    }
+    free(tt);
+    tt = NULL;
+}
+
+struct tipo *tt_insertar_tipo(struct list_head *tt,
+                              enum TT tipo,
+                              struct tipo *base,
+                              u16 tam,
+                              u8 dim)
+{
+    if (tt == NULL)
+        return NULL;
+    struct tipo *nuevo = malloc(sizeof(struct tipo));
+    if (nuevo == NULL)
+        return NULL;
+    nuevo->id =
+        list_empty(tt) ? 0 : list_first_entry(tt, struct tipo, list)->id + 1;
+    nuevo->tipo = tipo;
+    nuevo->base = base;
+    nuevo->dim = dim;
+    nuevo->tam = dim > 0 ? dim * base->tam : tam;
+    list_add(&nuevo->list, tt);
+    return nuevo;
+}
+
+struct tipo *tt_buscar_id(struct list_head *tt, u16 id)
+{
+    if (tt == NULL || list_empty(tt))
+        return NULL;
+    struct tipo *tipo;
+    list_for_each_entry(tipo, tt, list)
+    {
+        if (tipo->id == id) {
+            return tipo;
+        }
+    }
+    return NULL;
+}
+
+void tt_imprimir_tipo(struct tipo *tipo)
+{
+    if (tipo == NULL) {
+        printf("Tipo nulo\n");
+        return;
+    }
+    printf("id: %lu,\t tipo: %u,\t base:%lu,\t tam: %lu,\t dim:%d\n", tipo->id,
+           tipo->tipo, tipo->base == NULL ? 0 : tipo->base->id, tipo->tam,
+           tipo->dim);
+}
+
+void tt_imprimir_tabla(struct list_head *tt)
+{
+    struct tipo *tipo;
+    printf("Tabla de tipos\n");
+    if (tt == NULL || list_empty(tt)) {
+        printf("Vacia\n");
+        return;
+    }
+    list_for_each_entry_reverse(tipo, tt, list) { tt_imprimir_tipo(tipo); }
+}
