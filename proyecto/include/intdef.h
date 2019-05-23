e660a1cc78cfcce9f7ad64d78ee2c370392e578f
Emilio Cabrera <emilio1625@gmail.com>
2019-05-23 10:29:05 -0500

    Cabecera faktante - definiciones de enteros

---
+++ proyecto/include/intdef.h
@@ -0,0 +1,13 @@
+#ifndef INTDEF_H
+#define INTDEF_H
+
+#include <stdint.h>
+
+typedef int_fast8_t u8;
+typedef uint_fast8_t s8;
+typedef uint_fast16_t u16;
+typedef int_fast16_t s16;
+typedef uint_fast32_t u32;
+typedef uint_fast32_t s32;
+
+#endif /* end of include guard: INTDEF_H */
