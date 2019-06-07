00d9f34c998413be4772ec34ff804c1851187aba
Emilio Cabrera <emilio1625@gmail.com>
2019-06-07 00:35:06 -0500

    Herramientas para el compilador

---
+++ proyecto/include/codigo_intermedio.h
@@ -0,0 +1,76 @@
+#ifndef CODIGO_INTERMEDIO_H
+#define CODIGO_INTERMEDIO_H
+
+#include "list.h"
+#include "string.h"
+#include "tools.h"
+
+struct code {
+    // la posicion en la lista que contiene el codigo
+    u16 dir;
+    char op[16], dir1[16], dir2[16], res[16];
+    struct list_head list;
+};
+
+struct code *code_push(struct list_head *head,
+                       char op[16],
+                       char dir1[16],
+                       char dir2[16],
+                       char res[16])
+{
+    if (head == NULL)
+        return NULL;
+    struct code *new = malloc(sizeof(struct code));
+    if (new == NULL)
+        return NULL;
+    strncpy(new->op, op, 16);
+    strncpy(new->dir1, dir1, 16);
+    strncpy(new->dir2, dir2, 16);
+    strncpy(new->res, res, 16);
+    new->dir = list_empty(head)
+                   ? 0
+                   : list_first_entry(head, struct code, list)->dir + 1;
+    list_add(&new->list, head);
+    return new;
+}
+
+struct list_head *combinar(struct list_head *head1, struct list_head *head2)
+{
+    if (head1 == head2) {
+        return head1;
+    } else if (head1 == NULL || list_empty(head1)) {
+        return head2;
+    } else if (head2 == NULL || list_empty(head2)) {
+        return head1;
+    } else {
+        list_splice_init(head2, head1);
+    }
+    return head1;
+}
+
+void backpatch(struct list_head *code, struct list_head *dirs, char label[16])
+{
+    if (dirs == NULL || list_empty(dirs) || code == NULL || list_empty(code) ||
+        label == NULL)
+        return;
+    u32 i              = 0;
+    struct code *tmp_c = list_entry(code->next, struct code, list);
+    struct dir_entry *tmp_d;
+    // esperemos que funcione
+    // TODO: arreglar esta cochinada
+    list_for_each_entry_reverse(tmp_d, dirs, list)
+    {
+        if (i == tmp_d->dir) {
+            strncpy(tmp_c->res, label, 16);
+            continue;
+        }
+        list_for_each_entry_continue(tmp_c, code, list)
+        {
+            i++;
+            if (i >= tmp_d->dir)
+                break;
+        }
+    }
+}
+
+#endif /* end of include guard: CODIGO_INTERMEDIO_H */
---
+++ proyecto/include/tools.h
@@ -0,0 +1,119 @@
+#ifndef TOOLS_H
+#define TOOLS_H
+
+#include <stdio.h>
+#include "list.h"
+#include "stack.h"
+#include "tabla_simbolos.h"
+#include "tabla_tipos.h"
+
+///////////////////////////////////////////////////////////////////////////////
+//                               Etiquetas                                   //
+///////////////////////////////////////////////////////////////////////////////
+char *etiqueta_nueva()
+{
+    char *str = malloc(16 * sizeof(char));
+    static const char set[] =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    sprintf(str, "label_");
+
+    for (int i = 6; i < 16; ++i) {
+        str[i] = set[rand() % (sizeof(set) - 1)];
+    }
+
+    str[16] = '\0';
+    return str;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//                         Stack de direciones                               //
+///////////////////////////////////////////////////////////////////////////////
+struct dir {
+    u16 dir;
+    struct list_head list;
+};
+
+void dir_push(struct list_head *stack, u16 dir)
+{
+    if (stack == NULL)
+        return;
+    struct dir *new = malloc(sizeof(struct dir));
+    if (new == NULL)
+        return;
+    new->dir = dir;
+    list_add(&new->list, stack);
+}
+
+u16 dir_pop(struct list_head *stack)
+{
+    if (stack == NULL || list_empty(stack))
+        return -1;
+    struct dir *entry = list_first_entry(stack, struct dir, list);
+    list_del(&entry->list);
+    u16 dir = entry->dir;
+    free(entry);
+    return dir;
+}
+
+u16 dir_peek(struct list_head *stack)
+{
+    if (stack == NULL || list_empty(stack))
+        return -1;
+    struct dir *entry = list_first_entry(stack, struct dir, list);
+    u16 dir           = entry->dir;
+    return dir;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//                                 Ambito                                    //
+///////////////////////////////////////////////////////////////////////////////
+void ambito_crear(struct list_head *tt_s,
+                  struct list_head **tt,
+                  struct list_head *ts_s,
+                  struct list_head **ts,
+                  struct list_head *dir_s,
+                  u16 *dir)
+{
+    /* Creando una nueva tabla de tipos */
+    stack_push(tt_s, tt_crear_tabla());
+    *tt = stack_peek(tt_s);
+    if (tt == NULL) {
+        printf("Error creando la tabla de tipos\n");
+    }
+
+    /* Creando una nueva tabla de simbolos */
+    stack_push(ts_s, ts_crear_tabla());
+    *ts = stack_peek(ts_s);
+    if (ts == NULL) {
+        printf("Error creando la tabla de simbolos\n");
+    }
+
+    /* Guadando la direccion anterior y regresando a 0 la actual */
+    dir_push(dir_s, 0);
+    *dir = dir_peek(dir_s);
+}
+
+void ambito_restaurar(struct list_head *tt_s,
+                      struct list_head **tt,
+                      struct list_head *ts_s,
+                      struct list_head **ts,
+                      struct list_head *dir_s,
+                      u16 *dir,
+                      int eliminar)
+{
+    stack_pop(ts_s);
+    if (eliminar)
+        ts_eliminar_tabla(ts);
+    *ts = stack_peek(ts_s);
+
+    stack_pop(tt_s);
+    if (eliminar)
+        tt_eliminar_tabla(tt);
+    *ts = stack_peek(tt_s);
+
+    dir_pop(dir_s);
+    *dir = dir_peek(dir_s);
+}
+
+#endif /* end of include guard: TOOLS_H */
